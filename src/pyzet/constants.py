d9f22743b5c3caf9b75c16998a494ae7c8e5aeb8
Tomasz Wojdat <tpwojdat@gmail.com>
2021-10-16 16:20:33 +0200

    Fix `importlib.metadata` import on Python < 3.8
    Use "pragma: no cover" with given Python versions.

--- src/pyzet/constants.py
+++ src/pyzet/constants.py
@@ -1,3 +1,8 @@
-import importlib.metadata as importlib_metadata
+import sys
+
+if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
+    import importlib_metadata
+else:  # pragma: no cover (PY38+)
+    import importlib.metadata as importlib_metadata
 
 VERSION = importlib_metadata.version("pyzet")
