0890ef3fa4cc842083784a18bb3dee0d3a3c9507
Tomasz Wojdat <wojdatto@gmail.com>
2021-10-16 16:17:04 +0200

    Add version number to setup.cfg and to the CLI
    The first version number is 0.1.0.
    
    Versions with leading `0` indicate project that has no final version of
    its public API. Semantic Versioning specs advise to use `0.1.0` as the
    first semantic version of any project that wants to follow it.

---
+++ src/pyzet/constants.py
@@ -0,0 +1,3 @@
+import importlib.metadata as importlib_metadata
+
+VERSION = importlib_metadata.version("pyzet")
--- src/pyzet/main.py
+++ src/pyzet/main.py
@@ -1,8 +1,19 @@
 import argparse
 from typing import List, Optional
 
+import pyzet.constants as C
+
 
 def main(argv: Optional[List[str]] = None) -> int:
     parser = argparse.ArgumentParser(prog="pyzet")
+
+    # https://stackoverflow.com/a/8521644/812183
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"%(prog)s {C.VERSION}",
+    )
+
     parser.parse_args(argv)
     return 0
