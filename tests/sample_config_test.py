6a5bd5bb3c9925c64ebec7e7e97549b252aa0686
Tomasz Wojdat <wojdatto@gmail.com>
2022-01-25 00:40:03 +0100

    Add support for YAML config file
    It now supports four fields: repo, editor, git, and grep. Only the first
    one is required to be present. Hard-coded default values are used when
    other fields are not defined.
    
    Executable paths defined in repo are treated as `str` which is converted
    to `pathlib.Path` just before calling them. The actual looking for
    executable is done with the help of `shutil.which()` that is able to
    also look for it on `PATH`.
    
    Other changes from this commit:
    
    * Add `sample-config` command
    
      A config file can be easily created by redirecting stdout to a file.
    
        I took the inspiration here from pre-commit:
        https://github.com/pre-commit/pre-commit/blob/master/pre_commit/commands/sample_config.py
    
    * Update `Config` dataclass:
    
        * Remove default values. Use get method to check if data is in YAML.
          If not, use the default value (apart from repo path that always
          needs to be set).
    
        * Rename `repo_path` to `repo` in Config.
    
    * Go back to the single test variant for grep, and mark it as skipped,
      because grep behaves differently in Linux and Git Bash on Windows.
    
    Also:
    
    * Add error message where there is no config file present
    * Add error message when `pyzet edit` is called when there are no zettels
    * Add tests for already existing `init` command
    * Update summary of commands in readme
    
    Close #13.

---
+++ tests/sample_config_test.py
@@ -0,0 +1,27 @@
+from pyzet.sample_config import sample_config
+
+
+def test_sample_config(capsys):
+    ret = sample_config()
+    assert ret == 0
+    out, _ = capsys.readouterr()
+
+    # The assertion is split, because there is one line which is generated dynamically.
+    assert out.startswith(
+        """\
+# See https://github.com/wojdatto/pyzet.git for more information.
+#
+# Put this file at\
+"""
+    )
+
+    assert out.endswith(
+        """\
+# Below options use global paths, but feel free
+# to use program name directly if it's on your PATH.
+repo: ~/zet
+editor: /usr/bin/vim
+git: /usr/bin/git
+grep: /usr/bin/grep
+"""
+    )
