907f39116204d387eb45a382e469232a7967a73a
Tomasz Wojdat <wojdatto@gmail.com>
2022-10-13 23:40:10 +0200

    Remove duplicated sample-config header

--- src/pyzet/sample_config.py
+++ src/pyzet/sample_config.py
@@ -1,22 +1,21 @@
 import pyzet.constants as C
 
-SAMPLE_CONFIG_UNIX = f"""\
+_header = f"""\
 # See https://github.com/wojdatto/pyzet for more information.
 #
 # Put this file at {C.DEFAULT_CFG_LOCATION.as_posix()}
 # Below options use global paths, but feel free
-# to use program name directly if it's on your PATH.
+# to use program name directly if it's on your PATH."""
+
+SAMPLE_CONFIG_UNIX = f"""\
+{_header}
 repo: ~/zet
 editor: {C.vim_unix_path}
 git: {C.git_unix_path}
 """
 
 SAMPLE_CONFIG_WINDOWS = f"""\
-# See https://github.com/wojdatto/pyzet for more information.
-#
-# Put this file at {C.DEFAULT_CFG_LOCATION.as_posix()}
-# Below options use global paths, but feel free
-# to use program name directly if it's on your PATH.
+{_header}
 repo: ~/zet
 editor: {C.vim_win_path}
 git: {C.git_win_path}
