04987902a6559fcd4c0c438bd4e563497c33c60e
Tomasz Wojdat <tpwojdat@gmail.com>
2023-11-02 22:18:12 +0100

    Automatically include future annotations in Python sourcefiles

--- src/pyzet/__main__.py
+++ src/pyzet/__main__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pyzet.main import main
 
 if __name__ == '__main__':
--- testing/constants.py
+++ testing/constants.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pyzet.constants as C
 
 TEST_CFG = ('--config', f'testing/{C.CONFIG_FILE}')
